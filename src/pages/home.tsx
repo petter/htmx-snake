fc4f164c030d86fed2d2232df762f48e24f0fcd2
Petter Sæther Moen <pettersmoen@gmail.com>
2024-04-09 16:31:13 +0200

    prettier keyboard buttons

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,15 +7,22 @@ export function HomePage() {
         <div class="border-green-500 border-4">
           <div hx-ext="sse" sse-connect="/snake" sse-swap="tick"></div>
         </div>
-        <div>
-          {["Up", "Down", "Left", "Right"].map((dir) => (
+        <div class='grid gap-2 [grid-template-areas:"._up_."_"left_down_right"]'>
+          {[
+            { dir: "Up", icon: "^" },
+            { dir: "Down", icon: "v" },
+            { dir: "Left", icon: "<" },
+            { dir: "Right", icon: ">" },
+          ].map(({ dir, icon }) => (
             <button
               hx-post={`/${dir.toLowerCase()}`}
               hx-include="#snakeId, #snakeColor"
               hx-trigger={`click, keydown[key==='Arrow${dir}'] from:body`}
               hx-swap="none"
+              class="px-3 py-2 text-sm font-semibold text-gray-900 bg-white border border-gray-200 rounded-lg shadow-outer"
+              style={{ gridArea: dir.toLowerCase() }}
             >
-              {dir}
+              {icon}
             </button>
           ))}
         </div>
